1ba37c1328bd57b0349f6bbeb6986b7ac410ea55
sachin <sachinesky@gmail.com>
2026-03-09 19:07:20 +0530

    fix applicant field

--- src/RecruiterDashboard.js
+++ src/RecruiterDashboard.js
@@ -473,14 +473,14 @@ return (
                             <p><b>Status:</b> {app.status}</p>
 
                             <button
-                                    onClick={() => handleUpdateStatus(app.id, "SHORTLISTED", app.student)}
+                                    onClick={() => handleUpdateStatus(app.id, "SHORTLISTED", app.applicant)}
                                     style={{marginRight:"10px",background:"green",color:"white"}}
                                     >
                                     Shortlist
                                     </button>
 
                                     <button
-                                    onClick={() => handleUpdateStatus(app.id, "REJECTED", app.student)}
+                                    onClick={() => handleUpdateStatus(app.id, "REJECTED", app.applicant)}
                                     style={{background:"red",color:"white"}}
                                     >
                                     Reject
